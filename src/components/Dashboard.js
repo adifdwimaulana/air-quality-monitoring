c04f0adc601d07acd5ab05cea02dd1dadd0f9626
adifdwimaulana <adifdwimaulana@gmail.com>
2020-06-27 10:20:18 +0700

    Fix chart

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,58 +1,122 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import FusionCharts from 'react-native-fusioncharts';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
+import { LineChart } from "react-native-chart-kit";
 
-export default class Dashboard extends Component {
+let dataArr = [50, 79, 120, 66];
+let labelArr = ["Senin", "Selasa", "Rabu", "Kamis"]
+const dayArray = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+const monthArray = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
+
+let day = new Date().getDay();
+let date = new Date().getDate();
+let month = new Date().getMonth();
+month = monthArray[month]
+const year = new Date().getFullYear();
+console.log(month)
+
+if (date < 10) {
+    date = '0' + date;
+}
+
+if (month < 10) {
+    month = '0' + month;
+}
+
+let currentDay = dayArray[day];
+let today = date + ' ' + month + ' ' + year;
+let currentDate = date + '-' + month + '-' + year;
+
+class Dashboard extends React.Component {
     constructor(props) {
         super(props);
-        this.apiCaller = null;
+
         this.state = {
-            type: 'angulargauge',
-            width: '100%',
-            height: '100%',
-            dataFormat: 'json',
-            dataSource: '../data.json'
-        };
-        this.libraryPath = Platform.select({
-            // Specify fusioncharts.html file location
-            android: { uri: 'file:///android_asset/fusioncharts.html' },
-            ios: require('./assets/fusioncharts.html')
-        });
+            data: null,
+            day: '',
+            date: '',
+            ispu: null
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ day: currentDay, date: currentDate })
     }
 
     render() {
+        const { data, day, date } = this.state
+        console.log(day)
+        console.log(date)
         return (
-            <View style={styles.container}>
-                <Text style={styles.header}>A Simple Gauge</Text>
+            <ScrollView style={styles.container}>
+                {/* <Text style={styles.title}>Bezier Line Chart</Text> */}
+                <Text style={styles.title}>Informasi Kualitas Udara</Text>
+                <Text style={styles.date}>{day}, {date}</Text>
                 <View style={styles.chartContainer}>
-                    <FusionCharts
-                        type={this.state.type}
-                        width={this.state.width}
-                        height={this.state.height}
-                        dataFormat={this.state.dataFormat}
-                        dataSource={this.state.dataSource}
-                        libraryPath={this.libraryPath}
+                    <LineChart
+                        data={{
+                            labels: labelArr,
+                            datasets: [
+                                {
+                                    data: dataArr
+                                }
+                            ]
+                        }}
+                        width={Dimensions.get("window").width * 0.94} // from react-native
+                        height={220}
+                        // yAxisLabel="$"
+                        // yAxisSuffix="k"
+                        yAxisInterval={1} // optional, defaults to 1
+                        chartConfig={{
+                            backgroundColor: "#094ab5",
+                            backgroundGradientFrom: "#1060e0",
+                            backgroundGradientTo: "#4a88ed",
+                            decimalPlaces: 0, // optional, defaults to 2dp
+                            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                            style: {
+                                borderRadius: 16,
+                            },
+                            propsForDots: {
+                                r: "6",
+                                strokeWidth: "2",
+                                stroke: "#ffa726"
+                            }
+                        }}
+                        bezier
+                        style={styles.chart}
                     />
                 </View>
-            </View>
-        );
+            </ScrollView>
+        )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 10
     },
-    header: {
+    title: {
+        fontSize: 28,
         fontWeight: 'bold',
-        fontSize: 20,
-        textAlign: 'center',
-        paddingBottom: 10
+        marginTop: 16,
+        marginLeft: 24,
+        letterSpacing: 0.8,
+        color: '#000'
     },
-    chartContainer: {
-        height: 400,
-        borderColor: '#000',
-        borderWidth: 1
+    date: {
+        fontSize: 12,
+        color: '#333',
+        marginTop: 4,
+        marginLeft: 24,
+        marginBottom: 10,
+        fontWeight: "600",
+        letterSpacing: 1.5
+    },
+    chart: {
+        marginVertical: 16,
+        borderRadius: 16,
+        alignItems: 'center',
     }
-});
+})
+
+export default Dashboard;
